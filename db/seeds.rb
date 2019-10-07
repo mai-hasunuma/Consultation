550f90eebc9a262e51704c30648fcc744707c509
蓮沼真愛 <hasumai@hasunumashinainoMacBook-Pro.local>
2019-10-07 12:15:52 +0900

    AddSeedFile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# coding: utf-8
+TroubleCategory.create(:name => '仕事と家庭の両立')
+TroubleCategory.create(:name => '家事全般')
+TroubleCategory.create(:name => '育児')
+TroubleCategory.create(:name => '嫁姑問題')
+TroubleCategory.create(:name => '仕事と育児の両立')
+TroubleCategory.create(:name => '貯蓄')
+TroubleCategory.create(:name => '介護')
+TroubleCategory.create(:name => 'ワンオペ育児')
+TroubleCategory.create(:name => '同居')
+TroubleCategory.create(:name => '受験')
+TroubleCategory.create(:name => 'いじめ')
