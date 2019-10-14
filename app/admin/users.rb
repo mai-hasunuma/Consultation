1971e52135ac0bf33c93aaeab10dc7e321f2f22c
mai-hasunuma <52441164+mai-hasunuma@users.noreply.github.com>
2019-10-14 13:24:59 +0900

    管理者画面実装中

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -7,6 +7,7 @@ ActiveAdmin.register User do
   #
   # permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :deleted_at, :name, :housewife_year, :image, :introduction
   permit_params :email, :password, :name, :housewife_year, :image, :introduction, :deleted_at
+  
   index do
     selectable_column
     id_column
