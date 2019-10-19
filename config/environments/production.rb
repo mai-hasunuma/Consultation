8a0251ba3cf0770dff0f6613cc8e4f99e50827ff
EC2 Default User <ec2-user@ip-172-31-33-102.ap-northeast-1.compute.internal>
2019-10-19 05:39:51 +0000

    デプロイ用に修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,8 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  config.action_cable.allowed_request_origins = [ 'http://xx.xx.xx.xx/' ]
+  #config.action_cable.allowed_request_origins = [ 'http://xx.xx.xx.xx/' ]
+  config.action_cable.allowed_request_origins = [ 'http://54.65.121.14' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
