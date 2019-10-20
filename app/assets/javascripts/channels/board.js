aa9608e15d92c612dfc5e5a721fa4f1563b2ee22
mai-hasunuma <mai.tomimura@gmail.com>
2019-10-20 08:02:19 +0000

    DM詳細画面プロフィール画像変更

--- app/assets/javascripts/channels/board.js
+++ app/assets/javascripts/channels/board.js
@@ -45,10 +45,12 @@ App.board = App.cable.subscriptions.create("BoardChannel", {
     var newUserImage = document.createElement('img');
     if(data.image){
       newUserImage.src = data.image;
+      newUserImage.width = 50;
+      newUserImage.height = 50;
     }else{
       newUserImage.src = "/no_image.png";
-      newUserImage.width = 40;
-      newUserImage.height = 40;
+      newUserImage.width = 50;
+      newUserImage.height = 50;
     };
 
     col1.appendChild(newUserImage);
