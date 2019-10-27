b7d6c8e08cc2555373c46f4a29eed2a30281f046
mai-hasunuma <mai.tomimura@gmail.com>
2019-10-27 11:43:25 +0000

    会員登録後の画面遷移を変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get 'notifications/index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations"
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # home
   root 'home#top'
