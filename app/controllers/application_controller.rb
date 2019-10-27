b198c003259e0443d13db0bae53fab3f7950e606
mai-hasunuma <mai.tomimura@gmail.com>
2019-10-27 10:29:44 +0000

    会員登録画面更新

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
   def after_sign_in_path_for(resource)
     users_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [ :email, :name, :password, :password_confirmation, :housewife_year ]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
   end
 end
