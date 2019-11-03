b0f4184a585220e4a6181ac87a29f5059e882a7f
mai-hasunuma <mai.tomimura@gmail.com>
2019-11-03 03:53:41 +0000

    コード修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,27 +20,10 @@ class UsersController < ApplicationController
       @user.past_troubles.find_or_initialize_by(user_id: @user.id)
       render 'edit'
     end
-      # current_trouble_category_idとpast_trouble_category_idを受け渡しているので下記の記述は不要となる
-      # ここから
-      # params[:user][:current_troubles][:trouble_category_ids].each do |current_trouble|
-      #   a = CurrentTrouble.new
-      #   a.user_id = current_user.id.to_i
-      #   a.trouble_category_id = current_trouble.to_i
-      #   a.save!
-      # end
-
-      # params[:user][:past_troubles][:trouble_category_ids].each do |past_trouble|
-      #   b = PastTrouble.new
-      #   b.user_id = current_user.id.to_i
-      #   b.trouble_category_id = past_trouble.to_i
-      #   b.save!
-      # end
-      # ここまで
   end
 
   def show
     @user = User.find(params[:id])
-    # binding.pry
     if user_signed_in?
       @currentUserEntry = Entry.where(user_id: current_user.id)
       @userEntry = Entry.where(user_id: @user.id)
@@ -91,7 +74,7 @@ class UsersController < ApplicationController
       @users = @q.result(distinct: true).page(params[:page]).per(10)
       return
     end
-    # binding.pry
+
     search_words = params[:q][:introduction_cont_any].split(/[,\n\p{blank}]+/)
     @q = User.ransack(housewife_year_gteq: params[:q][:housewife_year_gteq], housewife_year_lt: params[:q][:housewife_year_lt],
        introduction_cont_any: search_words, current_trouble_categories_id_in: params[:q][:current_trouble_categories_id_in], past_trouble_categories_id_in: params[:q][:past_trouble_categories_id_in])
@@ -113,7 +96,6 @@ class UsersController < ApplicationController
   end
 
   protected
-  # 追記する
   def update_resource(resource, params)
     resource.update_without_password(params)
   end
