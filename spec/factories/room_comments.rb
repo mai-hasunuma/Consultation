3c9ced1918f49cece7b2920fd05e4b4f3ddc6d94
mai-hasunuma <mai.tomimura@gmail.com>
2019-11-06 13:59:59 +0000

    RoomCommentモデルテスト完了

--- spec/factories/room_comments.rb
+++ spec/factories/room_comments.rb
@@ -3,5 +3,19 @@
 FactoryBot.define do
   factory :room_comment do
     sequence(:content) { |n| "content#{n}" }
+    association :user
+    association :room
+
+    trait :no_room_comment_room do
+      room {}
+    end
+
+    trait :no_room_comment_user do
+      user {}
+    end
+
+    trait :no_room_comment_content do
+      content {}
+    end
   end
 end
