c8def2e2eb573b8628b23198daec15e1c5e5a7dc
mai-hasunuma <mai.tomimura@gmail.com>
2019-11-06 04:38:39 +0000

    boardモデルテストほぼ完了

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -7,30 +7,54 @@ RSpec.describe Board, 'モデルに関するテスト', type: :model do
     it 'Userモデルに属している' do
       is_expected.to belong_to(:user)
     end
+
+    it 'BoardSelectedCategoryモデルを多数持っている' do
+      is_expected.to have_many(:board_selected_categories)
+    end
+
+    it 'BoardCommentモデルを多数持っている' do
+      is_expected.to have_many(:board_comments)
+    end
   end
 
   describe '実際に保存してみる' do
     context '保存できる場合' do
-      it 'user_idを入れて保存' do
+      it 'user_idをいれて保存' do
         user = FactoryBot.create(:user)
         expect(FactoryBot.create(:board, user_id: user.id)).to be_valid
       end
-      it 'contentなし' do
-        user = FactoryBot.create(:user)
-        expect(FactoryBot.create(:board, user_id: user.id)).to be_valid
+
+      it '画像データあり' do
+        board = FactoryBot.build(:board)
+        board.images.attach(io: File.open(Dir.pwd + '/spec/files/test.jpg'), filename: 'test.jpg', content_type: 'image/jpg')
+        expect(board.images).to be_an_instance_of(ActiveStorage::Attached::Many)
       end
-      it 'contentあり' do
-        user = FactoryBot.create(:user)
-        expect(FactoryBot.create(:board, user_id: user.id)).to be_valid
+
+      it '画像データなし' do
+        expect(FactoryBot.create(:board)).to be_valid
       end
     end
-    # context "保存できない場合" do
-    #   it "user_idを保存していない" do
-    #     expect(FactoryBot.build(:board)).to_not be_valid
-    #   end
-    #   it "titleが空欄" do
-    #     expect(FactoryBot.build(:board, :no_title)).to_not be_valid
-    #   end
-    # end
+
+    context '保存できない場合' do
+      it 'user_id保存していない' do
+        expect(FactoryBot.build(:board, :no_board_user)).to_not be_valid
+      end
+
+      it 'titleが空欄' do
+        expect(FactoryBot.build(:board, :no_board_title)).to_not be_valid
+      end
+
+      it 'titleが31文字以上' do
+        expect(FactoryBot.build(:board, :too_long_board_title)).to_not be_valid
+      end
+
+      it 'contentが空欄' do
+        expect(FactoryBot.build(:board, :no_board_content)).to_not be_valid
+      end
+
+      it 'contentが201文字以上' do
+        expect(FactoryBot.build(:board, :too_long_board_content)).to_not be_valid
+      end
+    end
   end
 end
--- spec/models/trouble_category_spec.rb
+++ spec/models/trouble_category_spec.rb
@@ -3,35 +3,27 @@
 require 'rails_helper'
 
 RSpec.describe TroubleCategory, 'モデルに関するテスト', type: :model do
+  describe 'アソシエーション' do
+    it 'current_troubleモデルを複数持っている' do
+      is_expected.to have_many(:current_troubles)
+    end
+
+    it 'past_troubleモデルを複数持っている' do
+      is_expected.to have_many(:past_troubles)
+    end
+  end
+
   describe '実際に保存してみる' do
-    # 保存できるときは無し
-    # context "保存できない場合" do
-    #   it "nameが空欄" do
-    #     expect(FactoryBot.build(:user, :no_name)).to_not be_valid
-    #   end
+    context '保存できる場合' do
+      it 'nameあり' do
+        expect(FactoryBot.create(:trouble_category)).to be_valid
+      end
+    end
 
-    #   it "nameが31文字以上" do
-    #     expect(FactoryBot.build(:user, :too_long_name)).to_not be_valid
-    #   end
-
-    #   it "主婦歴が空欄" do
-    #     expect(FactoryBot.build(:user, :no_housewife_year)).to_not be_valid
-    #   end
-
-    #   it "メールアドレスが空欄" do
-    #     expect(FactoryBot.build(:user, :no_email)).to_not be_valid
-    #   end
-
-    #   it "重複したメールアドレス" do
-    #     FactoryBot.create(:user, email: "test1@example.com")
-
-    #     user = FactoryBot.build(:user, email: "test1@example.com")
-    #     user.valid?
-    #     expect(user.errors[:email]).to include("はすでに存在します")
-    #   end
-
-    #   it "introductionが301文字以上" do
-    #     expect(FactoryBot.build(:user, :too_long_introduction)).to_not be_valid
-    #   end
+    context '保存できない場合' do
+      it 'nameが空欄' do
+        expect(FactoryBot.build(:trouble_category, :no_trouble_cateroy_name)).to_not be_valid
+      end
+    end
   end
 end
