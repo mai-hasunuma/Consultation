26352cef89463e31e5818dd30cb0b9a35a76c009
mai-hasunuma <mai.tomimura@gmail.com>
2019-11-06 13:47:41 +0000

    Entryモデルテスト完了

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -3,31 +3,32 @@
 require 'rails_helper'
 
 RSpec.describe Entry, 'モデルに関するテスト', type: :model do
-  # describe 'アソシエーション' do
-  #   it "Userモデルに属している" do
-  #     is_expected.to belong_to(:user)
-  #   end
-
-  #   it "部屋に属している"
-  #     is_expected.to belong_to(:room)
-  # end
+  describe 'アソシエーション' do
+    it 'Userモデルに属している' do
+      is_expected.to belong_to(:user)
+    end
+    it 'Roomモデルに属している' do
+      is_expected.to belong_to(:room)
+    end
+  end
 
   describe '実際に保存してみる' do
     context '保存できる場合' do
-      it 'user_idを入れて保存' do
+      it 'user_id,room_idを入れて保存' do
+        room = FactoryBot.create(:room)
         user = FactoryBot.create(:user)
-        expect(FactoryBot.create(:entry, user_id: user.id)).to be_valid
-      end
-
-      it 'room_idを入れて保存' do
-        user = FactoryBot.create(:room)
-        expect(FactoryBot.create(:entry, user_id: user.id)).to be_valid
+        expect(FactoryBot.create(:entry, user_id: user.id, room_id: room.id)).to be_valid
       end
     end
-    # context "保存できない場合" do
-    #   it "user_id、entry_idを保存していない" do
-    #     expect(FactoryBot.build(:centry)).to_not be_valid
-    #   end
-    # end
+
+    context '保存できない場合' do
+      it 'user_idを保存していない' do
+        expect(FactoryBot.build(:entry, :no_entry_user)).to_not be_valid
+      end
+
+      it 'room_idを保存していない' do
+        expect(FactoryBot.build(:entry, :no_entry_room)).to_not be_valid
+      end
+    end
   end
 end
